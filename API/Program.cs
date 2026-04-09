a66b59ebcb9c64817ffe8d83b8180a3beb51a300
Wasif Ahmad <wasifaq@gmail.com>
2026-04-09 20:47:53 +0300

    End of Section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using API.Data;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -35,6 +36,8 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 // Middleware
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
